c095511e17df4fcb102c7873456e3a4c1463e960
Danijel <developedbydane@gmail.com>
2023-12-04 18:56:11 +0100

    style: minor font changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import '../styles/Reset.css'
 export function Footer(){
     return(
         <footer>
-            <h4>This website was built as a demo project by <a href="https://github.com/developedbydan/developedbydan.github.io">Danijel Guzvic</a></h4>
+            <h4>Brought to you by <a href="https://github.com/developedbydan/developedbydan.github.io">Danijel Guzvic</a></h4>
         </footer>
     )
 }
